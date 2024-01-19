00e897aa6fc5107411881a7d4df89b8491ba7b52
Ashton Wong <ashton@ucsb.edu>
2024-01-18 18:00:14 -0800

    changed test

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from ashton!";
+        return "Greetings from ashton-wong!";
     }
     
 }
